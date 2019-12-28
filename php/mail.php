c591e38cae86908a43550f0e72c9b08cd328c418
George Kara <seijindemon@protonmail.com>
2019-12-28 13:12:37 +0200

    fix send email!

--- php/mail.php
+++ php/mail.php
@@ -1,16 +1,15 @@
 <?php
-if(isset( $_POST['name']))
-$name = $_POST['name'];
-if(isset( $_POST['email']))
-$email = $_POST['email'];
-if(isset( $_POST['comments']))
-$message = $_POST['comments'];
-if(isset( $_POST['subject']))
-$subject = $_POST['subject'];
+if(isset($_POST['submit']))
+{
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['comments'];
 
-$content="From: $name \n Email: $email \n Message: $message";
-$recipient = "seijindemon@protonmail.com";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $content, $mailheader) or die("Error!");
-echo "Email sent!";
-?>
+    $mailTo = "georgekara2010@yahoo.gr";
+    $headers = "From: ".$email;
+    $txt = "From: $name \n Email: $email \n Message: \n $message";
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location:http://www.seijind.eu");
+}
