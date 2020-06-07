74015e1619c3ec26e0695e1c7c3af8b588ce64fb
George Kara <seijindemon@protonmail.com>
2020-06-07 16:50:22 +0300

    add carousel, add skills,projects, add dev link and many small changes

--- js/custom.js
+++ js/custom.js
@@ -63,3 +63,50 @@ function validateForm2()
     document.getElementById('status').innerHTML = "Στάλθηκε...";
     document.getElementById('contact-form').submit();
 }
+
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user=getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+        user = prompt("Please fill your name:","");
+        if (user != "" && user != null) {
+            setCookie("username", user, 30);
+        }
+    }
+}
+
+function checkCookie2() {
+    var user=getCookie("username");
+    if (user != "") {
+        alert("Καλώς ήρθατε ξανά " + user);
+    } else {
+        user = prompt("Παρακαλώ συμπληρώστε το όνομα σας:","");
+        if (user != "" && user != null) {
+            setCookie("username", user, 30);
+        }
+    }
+}
